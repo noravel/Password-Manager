de0937a71c21a530b35ffb097e340bfd81d5c543
noravel <nora.nganle@gmail.com>
2024-09-04 11:50:08 -0400

    adding password checker function

--- password_manager.py
+++ password_manager.py
@@ -1,21 +1,8 @@
 
-'''
-1. import libraries
-- hashlib: provides a helper function for efficient hashing of a file
-or file-like object. Return a digest object that has been updated with contents of file objetct. 
-=> encrypt the password
-- getpass library: to prompt a user for a password without echoing what they type on the screen. 
-
-'''
-
 import hashlib
 import getpass
+import re
 
-# a dictionary holds key-value pair : username-password hash
-password_manager = {}
-
-# prompt menu:
-'''
 def options():
     while True: 
         choice = input("Enter 1 if you are a new user\nEnter 2 if you are an existing user\nEnter 0 to exit: ")
@@ -23,29 +10,48 @@ def options():
             register()
         elif choice =='2':
             log_in()
+        elif choice =='3':
+            find_password()
         elif choice =='0':
             print('EXIT!')
-            break # need to fix this bug also
+            break
         else:
             print("Invalid value.")
-'''
 
-
-# create register():
 def register():
+
     username = input('Enter username: ')
     # getpass.getpass() prints a prompt then reads input from the user
     password = getpass.getpass('Enter your password: ')
-    # create a function called check_password: this function will
-    # check the strong of password. if it is strong, hash it
-    # else, ask user to create a stronger one
-    # 
-    encrypted_password = hashlib.sha256(password.encode()).hexdigest()
+    
+    while True:
+        if password_checker(password) == True:
+            print("Your password is strong")
+            break
+        else:
+            print("Your password is weak. try again")
+            new_password = getpass.getpass('Enter new password: ')
+            password = new_password
+
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
     # add the key-value pair : username-encrypted_password to the dicionary1
-    password_manager[username] = encrypted_password
+    password_manager[username] = hashed_password
     print("Congrats! Your account has been sucessfully created.")
 
-# create log_in():
+
+def password_checker(password):
+    # password: length: 8-20, at least 1 num, lower case , uppercase, special character.
+    # Repeated matches: 
+    checker = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W){8,16}$'
+
+    if re.fullmatch(checker, password):
+        print("Pass the test")
+        return True
+    else: 
+        print("Fail! Try stronger password.")
+        return False
+
+
 def log_in():
     username = input("Enter your username: ")
     password = getpass.getpass('Enter your password: ')
@@ -56,23 +62,8 @@ def log_in():
            print("Login sucessful")
     else:
         print("Username or password is incorrect. Try again!")
-
-
-def options():
-    while True: 
-        choice = input("Enter 1 if you are a new user\nEnter 2 if you are an existing user\nEnter 0 to exit: ")
-        if choice =='1':
-            register()
-        elif choice =='2':
-            log_in()
-        elif choice =='0':
-            print('EXIT!')
-            break # need to fix this bug also
-        else:
-            print("Invalid value.")
-
-
     
+
 if __name__ == "__main__":
     # a dictionary holds key-value pair : username-password hash
     password_manager = {}
